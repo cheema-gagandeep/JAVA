08092c56324f9d72477d6bdd52b518cb2026237a
gagandeep singh cheema <105306167+cheema-gagandeep@users.noreply.github.com>
2022-05-16 22:21:26 +0530

    BAsics of Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,12 @@
+class Inheritance{
+public static void main(String[] arg){
+    Derived d=new Derived();
+    d.num1=20;
+    d.baseshow();
+    d.num=10;
+    d.baseshow();
+    d.product();
+
+
+}
+}
